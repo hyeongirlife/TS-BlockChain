61c27bb56ab4e3a4b39e58086d1a0fd43c16a606
hyeongirlife <boo1996@naver.com>
2023-02-06 20:51:28 +0900

    Feat: [Config] tsconfig.json 세팅 중

---
+++ typechain/build/index.js
@@ -0,0 +1,12 @@
+var hyejin = function () { return "hyejin love hyeongeol"; };
+var Block = /** @class */ (function () {
+    function Block(data) {
+        this.data = data;
+    }
+    Block.hello = function () {
+        return "hi";
+    };
+    return Block;
+}());
+// const block = new Block("runa");
+// block.hello();
---
+++ typechain/src/index.ts
@@ -0,0 +1,10 @@
+const hyejin = () => "hyejin love hyeongeol";
+
+class Block {
+  constructor(private data: string) {}
+  static hello() {
+    return "hi";
+  }
+}
+// const block = new Block("runa");
+// block.hello();
